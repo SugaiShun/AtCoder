50e8f4c294f92ab8ce3cdf5b14de584300a96efa
SugaiShun <shun9121@gmail.com>
2021-05-11 00:47:23 +0900

    fix error

--- Bowls_and_Dishes.py
+++ Bowls_and_Dishes.py
@@ -1,17 +1,21 @@
-def check_condition(CD):
+def check_condition(dishes):
     condi_cnt = 0
     for i in range(M):
-        if CD[A[i]] > 0 and CD[B[i]]:
+        if dishes[A[i]] > 0 and dishes[B[i]] > 0:
             condi_cnt+=1
     return condi_cnt
 
 def tree_search(cd,depth,CD):
     dish = D[depth] if cd else C[depth]
-    CD.append(dish)
-    depth+=1
+
+    CD[dish]+=1
+    if sum(CD)>5:
+        print(" ")
     if depth == (K-1):
         return check_condition(CD)
-    return max(tree_search(0,depth,CD),tree_search(1,depth,CD))
+    depth+=1
+
+    return max(tree_search(0,depth,CD.copy()),tree_search(1,depth,CD.copy()))
 
 N,M = map(int,input().split())
 A = [0]*M
@@ -24,6 +28,7 @@ D = [0]*K
 for i in range(K):
     C[i],D[i] = map(int,input().split())
 
-CD = []
-max_cnt = max(tree_search(0,0,CD),tree_search(1,0,CD))
+CD1 = [0]*(N+1)
+CD2 = [0]*(N+1)
+max_cnt = max(tree_search(0,0,CD1),tree_search(1,0,CD2))
 print(max_cnt)
