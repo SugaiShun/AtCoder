5fe0b7ce97882026c7feeb5413fa282356deeec7
SugaiShun <shun9121@gmail.com>
2021-05-12 22:29:39 +0900

    fix staircase

--- staircase_sequences.py
+++ staircase_sequences.py
@@ -1,9 +1,9 @@
 def find_divisor(n):
-'''
-find divisor
-'''
+    '''
+    find divisor
+    '''
     res = []
-    i = 0
+    i = 1
     while i*i<n:
         if n%i==0:
             res.append(i)
@@ -13,6 +13,15 @@ find divisor
 
 
 def main():
-    return 0
+    N = int(input())
+
+    divs = find_divisor(2*N)
+    ans = 0
+    for div in divs:
+        m = (2*N)//div
+        if (div-m)%2 == 1:
+            ans+=1
+    
+    print(ans)
 
 main()
